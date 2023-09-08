1df9402d648d5dd2a3758e0fa1215f7300b59e67
Adam Griswold <adam.griswold@nucor.com>
2023-09-08 19:52:40 +0000

    ✨: init docs

---
+++ docs/README.md
@@ -0,0 +1,12 @@
+## Getting Started
+
+```bash
+pnpm link ../ # Setup shadcn-ui-layouts as link in node_modules
+pnpm run dev
+```
+
+## Technologies
+
+- NextJS
+- Nextra
+- Tailwindcss
---
+++ docs/theme.config.jsx
@@ -0,0 +1,8 @@
+export default {
+    logo: <span>shadcn-ui-layouts (Not affiliated with shadcn)</span>,
+    project: {
+      link: 'https://github.com/acgriswold/shadcn-ui-layouts'
+    }
+  }
+  
+  
